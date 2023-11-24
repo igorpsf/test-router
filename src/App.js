64e6e6c358e67775f5ad4de44ba7c3fb5d9b17fd
Igor Postnikov <igor.postnikov8@gmail.com>
2023-11-24 11:31:00 -0800

    test

--- src/App.js
+++ src/App.js
@@ -1,24 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter, Routes, Route, NavLink } from "react-router-dom";
+import Home from "./components/Home";
+import About from "./components/About";
+import Contact from "./components/Contact";
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <>
+        <BrowserRouter>
+          <div style={{
+            display: "flex",
+            background: 'black',
+            padding: '5px 0 5px 5px',
+            fontSize: '20px'
+          }}>
+            <div style={{ margin: '10px' }}>
+              <NavLink to="/" style={({ isActive }) => ({
+                color: isActive ? 'greenyellow' : 'white' })}>
+                Home
+              </NavLink>
+            </div>
+            <div style={{ margin: '10px' }}>
+              <NavLink to="/about" style={({ isActive }) => ({
+                color: isActive ? 'greenyellow' : 'white' })}>
+                About
+              </NavLink>
+            </div>
+            <div style={{ margin: '10px' }}>
+              <NavLink to="/contact" style={({ isActive }) => ({
+                color: isActive ? 'greenyellow' : 'white' })}>
+                Contact
+              </NavLink>
+            </div>
+          </div>
+          <Routes>
+            <Route exact path="/" element={<Home />} />
+            <Route exact path="/about" element={<About />} />
+            <Route exact path="/contact" element={<Contact />} />
+          </Routes>
+        </BrowserRouter>
+      </>
   );
 }
 
